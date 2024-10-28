9f13d8770ad14d728bd399d1ff8a6ecbc7bfcd8a
dishashetty546 <dishashetty546@gmail.com>
2024-10-28 21:04:45 +0530

    LEETCODECHALLENGE: FOURTYONE

--- LEETCODECHALLENGE/FOURTON.java
+++ LEETCODECHALLENGE/FOURTON.java
@@ -1,4 +1,6 @@
 package LEETCODECHALLENGE;
+
+import java.util.Stack;
 //super redudced string
 
 //brute force approach
@@ -9,14 +11,36 @@ package LEETCODECHALLENGE;
 //here we are iterating through the array every single time - increases time complexity
 
 //approach 2:
-//
+//using stack- used to find last char we encountered
+//ex: aaabccdd
+//push a into stack, then try to push a into stack, a alraedy present on the stack, pop a from stack....continues
 
 public class FOURTON {
     public static void main(String[] args) {
+        String str = "aaabccddd";
+        System.out.println(missing(str));
 
     }
 
-    public static void missing(String str) {
+    public static String missing(String str) {
+        // step 1: iniitialze a stack
+        Stack<Character> characterStack = new Stack<>();
+        // step 2: iterate through the string
+        // three conditions
+        // 1. if characterStack is empty, if so push the element from string onto stack
+        // 2. if charStack peek / previos element is equal to current element , then pop
+        // it
+        // 3. if is unique , then push into stack
+        for (char c : str.toCharArray()) {
+            if (characterStack.isEmpty()) {
+                characterStack.push(c);
+            } else if (c == characterStack.peek()) {
+                characterStack.pop();
+            } else {
+                characterStack.push(c);
+            }
+        }
+        return characterStack.toString();
 
     }
 
