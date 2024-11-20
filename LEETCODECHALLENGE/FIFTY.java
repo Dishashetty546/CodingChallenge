4f1714a2ea13e0b3bf4b4ef108f94b2d79dd4a14
dishashetty546 <dishashetty546@gmail.com>
2024-11-20 22:40:07 +0530

    LEETCODECHALLENGE

---
+++ LEETCODECHALLENGE/FIFTY.java
@@ -0,0 +1,27 @@
+package LEETCODECHALLENGE;
+
+import java.util.Arrays;
+// find duplicate in an array of elements
+
+// ex: 1,3,4,2,2
+// o/p: 2
+
+// brute force solution
+// take 1 element from array, ex: 1, search for 1 throughout, 1 is absent, hence
+// its unique of all elements, do that for all
+//
+
+// this approach is n't feasible, because it is iterating through array several
+// times
+// time complexity = "0(n*n)"
+// space complexity = "0(1)" //as we didnt use any extra space
+
+// efficient approach
+// approach 1: using sorting approach
+
+//
+// time complexity for sorting o(n log n)
+
+// more efficient approach
+
+// approach 2: usage hashset
