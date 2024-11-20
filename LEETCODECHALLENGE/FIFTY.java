d71f843a9d14b508ce8d0ebe5162d0c7164b611d
dishashetty546 <dishashetty546@gmail.com>
2024-11-20 22:43:39 +0530

    LEETCODECHALLENGE

--- LEETCODECHALLENGE/FIFTY.java
+++ LEETCODECHALLENGE/FIFTY.java
@@ -1,15 +1,38 @@
-package LEETCODECHALLENGE;
+// package LEETCODECHALLENGE;
+// //find duplicate in an array of elements
 
-import java.util.Arrays;
-// find duplicate in an array of elements
+// //ex: 1,3,4,2,2
+// //o/p: 2
 
-// ex: 1,3,4,2,2
-// o/p: 2
+// //brute force solution
+// //take 1 element from array, ex: 1, search for 1 throughout, 1 is absent,
+// hence its unique of all elements, do that for all
+// public class FIFTY {
+// static int k;
 
-// brute force solution
-// take 1 element from array, ex: 1, search for 1 throughout, 1 is absent, hence
-// its unique of all elements, do that for all
-//
+// public static void main(String[] args) {
+// int[] nums = { 2, 5, 4, 7, 5 };
+// System.out.println(duplicate(nums));
+
+// }
+
+// public static int duplicate(int[] nums) {
+// for (int i = 0; i < nums.length - 1; i++) {
+// for (int j = 1; j < nums.length - 1; j++)
+
+// if (nums[i] != nums[j]) {
+// i++;
+// j++;
+// } else {
+// k = nums[j];
+
+// }
+
+// }
+// return k;
+// }
+
+// }
 
 // this approach is n't feasible, because it is iterating through array several
 // times
@@ -19,9 +42,26 @@ import java.util.Arrays;
 // efficient approach
 // approach 1: using sorting approach
 
-//
-// time complexity for sorting o(n log n)
+// public class FIFTY {
+// public static void main(String[] args) {
+// int[] nums = { 3, 4, 5, 6, 2, 4 };
+// System.out.println((duplicate(nums)));
 
-// more efficient approach
+// }
 
-// approach 2: usage hashset
+// public static int duplicate(int[] nums) {
+// Arrays.sort(nums);
+
+// for (int i = 0; i < nums.length - 1; i++) {
+// if (nums[i] == nums[i + 1]) {
+// return nums[i];
+// }
+// }
+// return -1;
+
+// }
+// }
+
+// time complexity : o(n log n)
+
+// approach 2: using hashset
